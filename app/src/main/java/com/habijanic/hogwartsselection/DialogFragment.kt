c893cdef18e18bed4af88170d893abe8a6b81154
Amalia Habijanić <habijanic.ah@gmail.com>
2024-07-16 21:37:25 +0200

    Adding DialogFragment.kt and its design

---
+++ app/src/main/java/com/habijanic/hogwartsselection/DialogFragment.kt
@@ -0,0 +1,28 @@
+package com.habijanic.hogwartsselection
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.fragment.app.DialogFragment
+
+
+class DialogFragment : DialogFragment() {
+
+    lateinit var name : EditText
+    lateinit var cancel : Button
+    lateinit var ok : Button
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_dialog, container, false)
+    }
+
+
+}
